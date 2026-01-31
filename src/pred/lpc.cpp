a319b91cb1f10384f7a4aa922f1346dbf331f383
slmdev <quantkitty@outlook.de>
2026-01-31 16:07:27 +0100

    style

---
+++ src/pred/lpc.cpp
@@ -0,0 +1,42 @@
+#include "lpc.h"
+
+//#define INIT_COV
+
+OLS::OLS(int n,int kmax,double lambda,double nu,double beta_sum,double beta_pow,double beta_add)
+:x(n),
+chol(n),
+w(n),b(n),mcov(n,vec1D(n)),
+n(n),kmax(kmax),lambda(lambda),nu(n*nu),
+beta_pow(beta_pow),beta_add(beta_add),esum(beta_sum)
+{
+  km=0;
+  pred=0.0;
+  #ifdef INIT_COV
+    for (int i=0;i<n;i++) mcov[i][i]=1.0;
+  #endif
+}
+
+double OLS::Predict()
+{
+  pred=slmath::dot(x,w);
+  return pred;
+}
+
+void OLS::Update(double val)
+{
+  // update estimate of covariance matrix
+  esum.Update(fabs(val-pred));
+  double c0=pow(esum.Get()+beta_add,-beta_pow);
+
+  for (int j=0;j<n;j++) {
+    // only update lower triangular
+    for (int i=0;i<=j;i++) mcov[j][i]=lambda*mcov[j][i]+c0*(x[j]*x[i]);
+    b[j]=lambda*b[j]+c0*(x[j]*val);
+  }
+
+  km++;
+  if (km>=kmax) {
+    if (!chol.Factor(mcov,nu)) chol.Solve(b,w);
+    km=0;
+  }
+}
--- src/pred/lpc.h
+++ src/pred/lpc.h
@@ -4,49 +4,14 @@
 #include "../common/utils.h"
 #include "../common/math.h"
 
-//#define INIT_COV
 
 class OLS {
   public:
-    OLS(int n,int kmax=1,double lambda=0.998,double nu=0.001,double beta_sum=0.6,double beta_pow=0.75,double beta_add=2)
-    :x(n),
-    chol(n),
-    w(n),b(n),mcov(n,vec1D(n)),
-    n(n),kmax(kmax),lambda(lambda),nu(n*nu),
-    beta_pow(beta_pow),beta_add(beta_add),esum(beta_sum)
-    {
-      km=0;
-      pred=0.0;
-      #ifdef INIT_COV
-        for (int i=0;i<n;i++) mcov[i][i]=1.0;
-      #endif
-    }
-    double Predict()
-    {
-      pred=slmath::dot(x,w);
-      return pred;
-    }
-
-    void Update(double val)
-    {
-      // update estimate of covariance matrix
-      esum.Update(fabs(val-pred));
-      double c0=pow(esum.Get()+beta_add,-beta_pow);
-
-      for (int j=0;j<n;j++) {
-        // only update lower triangular
-        for (int i=0;i<=j;i++) mcov[j][i]=lambda*mcov[j][i]+c0*(x[j]*x[i]);
-        b[j]=lambda*b[j]+c0*(x[j]*val);
-      }
-
-      km++;
-      if (km>=kmax) {
-        if (!chol.Factor(mcov,nu)) chol.Solve(b,w);
-        km=0;
-      }
-    }
+    OLS(int n,int kmax=1,double lambda=0.998,double nu=0.001,double beta_sum=0.6,double beta_pow=0.75,double beta_add=2);
+    double Predict();
+    void Update(double val);
     vec1D x;
-  protected:
+  private:
     slmath::Cholesky chol;
     vec1D w,b;
     vec2D mcov;
